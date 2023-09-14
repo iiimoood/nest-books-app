e79bb64755cd7da85bef695e1b7dd1c22d0f0722
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-09-14 17:18:56 +0200

    Add new project, add files for authors and books

---
+++ src/books/dtos/update-book-dto.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Min,
+  Max,
+  IsUUID,
+} from 'class-validator';
+
+export class UpdateBookDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 100)
+  title: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  @Max(1000)
+  price: number;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @IsString()
+  authorId: string;
+}
