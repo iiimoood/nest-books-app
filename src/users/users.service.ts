484a7965bc209a87c8553f4144398d237933cdbd
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-09-19 20:19:39 +0200

    Add user and password models, add user module, controller and service, add methods in service

---
+++ src/users/users.service.ts
@@ -0,0 +1,99 @@
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<User[]> {
+    return this.prismaService.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        role: true,
+      },
+    });
+  }
+
+  public getById(id: User['id']): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+      },
+    });
+  }
+
+  public getByEmail(email: User['email']): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { email },
+      include: { password: true },
+    });
+  }
+
+  public async create(
+    userData: Omit<User, 'id'>,
+    hashedPassword: string,
+  ): Promise<User> {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...userData,
+          password: {
+            create: {
+              hashedPassword,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('User with this email already exists.');
+      throw error;
+    }
+  }
+
+  public async updateById(
+    userId: User['id'],
+    userData: Omit<User, 'id'>,
+    password: string | undefined,
+  ): Promise<User> {
+    try {
+      if (password) {
+        return await this.prismaService.user.update({
+          where: { id: userId },
+          data: {
+            ...userData,
+            password: {
+              update: {
+                hashedPassword: password,
+              },
+            },
+          },
+        });
+      } else {
+        return await this.prismaService.user.update({
+          where: { id: userId },
+          data: userData,
+        });
+      }
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new BadRequestException("User doesn't exist");
+    }
+  }
+
+  public deleteById(id: User['id']): Promise<User> {
+    return this.prismaService.user.delete({
+      where: { id },
+      include: { password: true },
+    });
+  }
+}
